7b8f117ecd2c5dbb1288ff26501000328caf5314
Matthew Winder <@mwinder>
2015-09-25 22:43:32 +0100

    Experiment with light patterns

--- starterkit-project-02.ino
+++ starterkit-project-02.ino
@@ -1,5 +1,5 @@
-
 int switchState = 0;
+int timeDelay = 750;
 
 void setup() {
   pinMode(3, OUTPUT);
@@ -8,22 +8,83 @@ void setup() {
   pinMode(2, INPUT);
 }
 
-void loop() {
+void loop(){
   switchState = digitalRead(2);
+
   if (switchState == LOW) { // button not pressed
-    digitalWrite(3, HIGH); // green
-    digitalWrite(4, LOW); // red 1
-    digitalWrite(5, LOW); // red 2
+    digitalWrite(5, LOW);  // red
+    digitalWrite(4, LOW);  // red
+    digitalWrite(3, LOW); // green
   }
-  else { // button pressed
-    digitalWrite(3, LOW);
+  else {
+    for (int i = 0; i < 21; i++)
+    {
+      digitalWrite(5, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(3, LOW);
 
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-    delay(250);
+      if (i % 3 == 0) {
+        digitalWrite(3, HIGH);
+      }
+      if (i % 3 == 1) {
+        digitalWrite(4, HIGH);
+      }
+      if (i % 3 == 2) {
+        digitalWrite(5, HIGH);
+      }
 
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-    delay(250);
+      delay(timeDelay);
+    }
   }
 }
+
+/*void loop() {
+  switchState = digitalRead(2);
+
+  if (switchState == LOW) { // button not pressed
+    digitalWrite(5, HIGH);  // red
+    digitalWrite(4, HIGH);  // red
+    digitalWrite(3, HIGH); // green
+  }
+  else {
+    digitalWrite(5, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(3, LOW);
+    delay(timeDelay);
+
+    digitalWrite(5, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(3, HIGH);
+    delay(timeDelay);
+
+    digitalWrite(5, LOW);
+    digitalWrite(4, HIGH);
+    digitalWrite(3, HIGH);
+    delay(timeDelay);
+
+    digitalWrite(5, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(3, LOW);
+    delay(timeDelay);
+
+    digitalWrite(5, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(3, LOW);
+    delay(timeDelay);
+
+    digitalWrite(5, LOW);
+    digitalWrite(4, HIGH);
+    digitalWrite(3, LOW);
+    delay(timeDelay);
+
+    digitalWrite(5, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(3, HIGH);
+    delay(timeDelay);
+
+    digitalWrite(5, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(3, LOW);
+    delay(timeDelay);
+  }
+}*/
